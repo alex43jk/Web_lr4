bc6b40648dad1c5eb3abad7699c843ae09dfabf2
Васильев Александр <leonbkone@yandex.ru>
2017-10-15 00:30:18 +0500

    Update and rename server.py to foo.py

---
+++ foo.py
@@ -0,0 +1,63 @@
+from webob import Request, Response
+
+req = Request.blank('/index.html')
+req.host = 'localhost'
+req.accept = 'text/html'
+req.http_version = 'HTTP/1.1'
+
+assets = [
+  'app.js',
+  'react.js',
+  'leaflet.js',
+  'D3.js',
+  'moment.js',
+  'math.js',
+  'main.css',
+  'bootstrap.css',
+  'normalize.css',
+]
+
+STYLES = []
+SCRIPTS = []
+
+for item in assets:
+    itemsplited = item.split('.')
+    if itemsplited[1] == 'js':
+        SCRIPTS.append(item)
+    elif itemsplited[1] == 'css':
+        STYLES.append(item)
+
+class WsgiTopBottomMiddleware(object):
+    def __init__(self, app):
+        self.app = app
+
+    def __call__(self, environ, start_response):
+      response = self.app(environ, start_response).decode()
+
+      if response.find('<head>' and '<body>') > -1:
+        htmlstart, head = response.split('<head>')
+        datahead, body = head.split('</head>')
+        head1, bodytag = body.split('<body>')
+        databody, end = bodytag.split('</body>')
+        
+        yield (htmlstart + data + end).encode() 
+      else:
+        yield (response).encode()
+
+def app(environ, start_response):
+  response_code = '200 OK'
+  response_type = ('Content-Type', 'text/HTML')
+  start_response(response_code, [response_type])
+  return ''''''
+
+app = WsgiTopBottomMiddleware(app)
+
+req2 = Request.blank('/index.html')
+
+from jinja2 import Environment, FileSystemLoader
+
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('index.html')
+print(template.render(javascripts=SCRIPTS, styles=STYLES))
+
+print(req2.get_response(app))
